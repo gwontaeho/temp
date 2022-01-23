4e633efdad023503ae9e851f1988a2bda68cbfa1
gwontaeho <taeho8199@gmail.com>
2022-01-23 17:28:52 +0900

    f

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const { sequelize } = require("./models");
 const api = require("./api");
 const app = express();
 
 app.set("port", process.env.PORT);
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static("public"));
