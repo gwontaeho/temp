fdf99ca365fe1d47181d635a701cc585e6c2ea71
gwontaeho <taeho8199@gmail.com>
2022-01-24 09:03:40 +0900

    f

--- aws/index.js
+++
@@ -1,7 +0,0 @@
-const aws = require("aws-sdk");
-aws.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
-exports.s3 = new aws.S3();
