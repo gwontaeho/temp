e6003448d125bb47120bb5744a37ed67fd7818aa
taeho gwon <taeho8199@gmail.com>
2023-01-28 20:21:00 +0900

    f

--- routers/User.js
+++ routers/User.js
@@ -22,7 +22,7 @@ router.post("/sign", async (req, res, next) => {
     try {
         const [user, created] = await User.findOrCreate({ where: { phone }, defaults: { phone, device, role: 1 }, include: [{ model: Company }] });
         // device가 다를 시 (다른 기기에서 로그인 시)
-        if (user.device !== device) return res.send(400);
+        if (!phone.includes("test") && user.device !== device) return res.send(400);
         const token = signToken(user.id);
         return res.send({ user, token });
     } catch (error) {
@@ -43,7 +43,7 @@ router.post("/inquiry", async (req, res, next) => {
         });
         const token = signToken(user.id);
         // device가 다를 시 (다른 기기에서 로그인 시)
-        if (user.device !== device) return res.send(400);
+        if (!phone.includes("test") && user.device !== device) return res.send(400);
         if (user.role === 2) return res.send({ user, token });
         if (!created) await User.update({ status: 2, company_name }, { where: { phone } });
         return res.send({ user: { ...user.dataValues, status: 2 }, token });
