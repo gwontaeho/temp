cd5ba5940669588762e2a67bf612e9e40b3cf28b
taeho gwon <taeho8199@gmail.com>
2023-02-07 22:35:52 +0900

    f

--- config/config.js
+++ config/config.js
@@ -15,9 +15,9 @@ const test = {
     dialect: "mysql",
 };
 const production = {
-    username: "root",
-    password: null,
-    database: "kys_production",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
     host: "127.0.0.1",
     dialect: "mysql",
 };
