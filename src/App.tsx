5f08bfcdb42cad436a62ac2bdfa60b78dc25ca49
unknown <taeho8199@gmail.com>
2023-12-08 09:04:39 +0900

    ;

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import axios, { InternalAxiosRequestConfig } from "axios";
 import RecoilProvider from "@/com/recoil";
 import { Main } from "@/com/routes/Main";
 import { Popup } from "@/com/routes/Popup";
 import { CommonModal, CommonToast } from "@/com/components/_";
 
 function App() {
-    useEffect(() => {
-        console.log("a");
-        axios.interceptors.request.use((config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
-            console.log(config);
-            return config;
-        });
-    }, []);
-
     return (
         <RecoilProvider>
             <BrowserRouter>
